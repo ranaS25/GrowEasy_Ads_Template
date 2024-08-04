c66cc707e8a5cf08d71044dd980039f96db056bc
My application <ranasankit46@gmail.com>
2024-08-04 23:16:22 +0530

    code optimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
 
 import Body from "@/components/Body";
-import EditBanner from "@/components/EditBanner";
-import { useState } from "react";
-
 
 export default function Home() {
-  // const [editFormOpen, setEditFormOpen] = useState(false)
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-start ">
-
-      {/* {editFormOpen && <EditBanner setEditFormOpen={setEditFormOpen} />} */}
-
-      {/* <button onClick={() => setEditFormOpen(!editFormOpen)
-       
-      } className="text-white font-bold">show editor</button> */}
-      
-
       <Body/>
-
     </main>
   );
 }
--- src/components/BannerContainer.tsx
+++ src/components/BannerContainer.tsx
@@ -14,7 +14,7 @@ const BannerContainer: React.FC = () => {
       <div className="w-full h-[100dvh] -z-10 fixed top-0 bg-gradient-to-br from-sky-400 to-blue-200  "></div>
       {editorOpened && <EditBanner />}
       <div className='w-fit  pt-10 min-h-96  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-4'>
-         
+
         {list1.map((item, index) => <Banner key={index} details={item} displayOnly={false} />)}
 
       </div>
