223a70b48e628d6853e53659520c5c150ca5fe68
Sankit rana <138496763+ranaS25@users.noreply.github.com>
2024-08-13 01:34:11 +0530

    updated a file

--- src/utils/Images.ts
+++ src/utils/Images.ts
@@ -4,14 +4,10 @@ export const imagesobject = {
   image3: "/images/image3.png",
   image4: "/images/image4.png",
   image5: "/images/image5.png",
-  image6: "/images/image6.png",
-  image7: "/images/image7.png",
-  image8: "/images/image8.png",
-  image9: "/images/image9.png",
-  image10: "/images/image10.png",
+
 };
 
 
 export const images = ["/images/image1.png", "/images/image2.png", "/images/image3.png"
-  , "/images/image4.png", "/images/image5.png", "/images/image6.png"
+  , "/images/image4.png", "/images/image5.png"
 ];
